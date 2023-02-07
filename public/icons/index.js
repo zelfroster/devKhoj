a92dcffd14e21d06e00b6dcff5d08383d4b15cc8
zelfroster <sumitkumarsoni123@gmail.com>
2023-02-08 05:14:06 +0530

    feat: refactor image locations

--- public/icons/index.js
+++ public/icons/index.js
@@ -4,7 +4,6 @@ import MailIcon from "./mail-icon.svg";
 import OrganisationIcon from "./organisation-icon.svg";
 import SearchIcon from "./search-icon.svg";
 import TwitterIcon from "./twitter-icon.svg";
-import LightModeIcon from "./lightMode.svg";
 import PeopleIcon from "./people-icon.svg";
 
-export { LinkIcon, LocationIcon, MailIcon, OrganisationIcon, SearchIcon, TwitterIcon, LightModeIcon, PeopleIcon }
+export { LinkIcon, LocationIcon, MailIcon, OrganisationIcon, SearchIcon, TwitterIcon, PeopleIcon }
